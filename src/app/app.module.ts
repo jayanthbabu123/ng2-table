1a30003e17b708af162edc4cefc8363f6d7b637f
jayanth <sjayanth.ip@gmail.com>
2018-02-20 01:44:30 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { Ng2TableModule } from 'ng2-table/ng2-table';
 
 import { AppComponent } from './app.component';
+import { GridComponent } from './grid/grid.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    Ng2TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
